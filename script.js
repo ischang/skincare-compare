b6100540318ea210dce9520b49f31aa5160445af
ischang <ischang@ucdavis.edu>
2019-03-19 17:37:31 -0700

    working js but need to fix alerts

--- script.js
+++ script.js
@@ -1,60 +1,99 @@
+var waterList = ["aqua", "eau", "water", "h20"];
+var goodWords = ["safe", "good", "yes"];
+var badWords = ["allergic", "bad", "no"];
+
 $(document).ready(function() {
   // The event listener for the file upload
   document.getElementById('csvFileUpload').addEventListener('input', uploadCsvFile, false);
 
-// Method that checks that the browser supports the HTML5 File API
+  // Method that checks that the browser supports the HTML5 File API
   function doesBrowserSupportFileUpload() {
     return window.File && window.FileReader && window.FileList && window.Blob;
   }
 
-// Method that reads and processes the selected file
+  // Method that reads and processes the selected file
   function uploadCsvFile(event) {
     if (!doesBrowserSupportFileUpload()) {
-      alert('Your browser and browser version is not compatible with the File API. Please refer to https://caniuse.com/#feat=filereader');
+      alert('Your browser/browser version is not compatible with the File API. Please refer to https://caniuse.com/#feat=filereader');
     } else {
       var data = null;
       var reader = new FileReader();
       reader.readAsText(event.target.files[0]);
 
-      reader.onload = function(csvData) {
+      reader.onload = function (csvData) {
         data = $.csv.toObjects(csvData.target.result);
-        
+
         if (data && data.length > 0) {
-        	console.log(data);
-        	handleData(data);
+          console.log(handleData(data));
           alert('Imported -' + data.length + '- rows successfully!');
         } else {
           alert('No data to import!');
         }
       };
 
-      reader.onerror = function() {
+      reader.onerror = function () {
         alert('Unable to read ' + file.fileName);
       };
     }
   }
 
-//option 1: to grab complete spreadsheet on both
-//option 2: grab spreadsheet with exclusions -- if the ingredient is on the "safe" list, then it will not occur on the "allergic" list
-//will only look for keywords of safe/allergic
-//if text has water in it, categorize it as water 
   function handleData(csvDataArray) {
-  	var safeList = new Object();
-  	var allergicList = new Object();
+    var safeList = new Object();
+    var allergicList = new Object();
 
-  	csvDataArray.forEach(function (csvObject) {
-  		if (csvObject["Result"]) {
-  			if(csvObject["Result"] == "Safe") {
+    try {
+      csvDataArray.forEach(function (csvObject) {
+        let result = csvObject["Result"].toLowerCase();
 
-  			} else if (csvObject["Result"] == "Allergic") {
+        if (result !== "tbd") {
+          if (goodWords.includes(result)) {
+            safeList = iterateThroughDictionary(csvObject["Ingredients"], safeList);
+          } else if (badWords.includes(result)) {
+            allergicList = iterateThroughDictionary(csvObject["Ingredients"], allergicList);
+          } else {
+            throw new Error();
+          }
+        }
+      });
+    } catch (error) {
+      alert("You a) might not have a \'Result\' column, b) misspelled your column, or c) one of your results is empty: please fill in \'safe\', \'good'\, \'yes\', \'no'\, \'bad\', \'allergic\', or \'tbd\'");
+    }
 
-  			} else {
-  				//
-  			}
-  		} 
-  	});
+    //option 2
+    for(let key in allergicList) {
+      if (key in safeList) {
+        delete allergicList[key];
+      }
+    }
 
+    //option 3
+    //just iterate through + add
+
+    return {
+      safeList: safeList,
+      allergicList: allergicList
+    };
   }
 
+  function iterateThroughDictionary(ingredients, dict) {
+    try {
+      let ingredientsArray = ingredients.split(/[ ,]+/);
 
-});
+      ingredientsArray.forEach(function (listIngredient) {
+        var ingredient = listIngredient.toLowerCase();
+
+        if (waterList.includes(ingredient)) {
+          ingredient = "water";
+        }
+
+        dict[ingredient] = dict[ingredient] ? dict[ingredient]+1 : 1;
+      });
+    } catch (error) {
+      alert("You may not have an \'Ingredients\' column.")
+      die(error);
+    }
+
+
+    return dict;
+  }
+})
