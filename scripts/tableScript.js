72fcd2f11d69e4a8b419f8c62c3a4986744bb696
ischang <ischang@ucdavis.edu>
2019-05-08 21:42:59 -0700

    added download buttons -- need to work on this

---
+++ scripts/tableScript.js
@@ -0,0 +1,56 @@
+function setDataAsTable(ingredientList, listName) {
+  var html = "<div class='table-responsive'>"
+  html += "<table class='table table-hover' style='width:60%'>";
+
+  html = setTableHeader(html);
+  html = setTableBody(html, ingredientList);
+
+  html+= "</table>";
+  html+="</div>";
+  listName = "." + listName + "Table";
+
+  $(listName).html(html);
+}
+
+function setTableHeader(html) {
+  html+="<thead style='font-weight:bold;'>";
+  html+="<tr>";
+  html+="<th>Ingredient</th>";
+  html+="<th>#</th>";
+  html+="</tr>";
+  html+="</thead>";
+
+  return html;
+}
+
+function setTableBody(html, ingredientList) {
+  html += "<tbody>";
+  var minList = sortIngredientListAsArray(ingredientList);
+
+  for (var i = 0; i < minList.length; i++) {
+    html+= "<tr>";
+    html+= "<td>" + minList[i][0] + "</td>";
+    html+= "<td>" + minList[i][1] + "</td>";
+    html+= "</tr>";
+  }
+
+  html+= "</tbody>";
+
+  return html;
+}
+
+function sortIngredientListAsArray(ingredientList) {
+  var items = Object.keys(ingredientList).map(function(key) {
+    return [key, ingredientList[key]];
+  });
+
+  items.sort(function(first, second) {
+    return second[1] - first[1];
+  });
+
+  return items.slice(0, setMinTableLength(ingredientList.length));
+}
+
+function setMinTableLength(listLength) {
+  return listLength < 10 ? listLength : 10;
+}
