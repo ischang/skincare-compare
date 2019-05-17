eada3fad2ee9dc7c17188613a85f287ad9a794c8
ischang <ischang@ucdavis.edu>
2019-05-17 16:28:04 -0700

    working basic commit

--- scripts/handleDataScript.js
+++ scripts/handleDataScript.js
@@ -7,39 +7,40 @@ function handleData(csvDataArray) {
   dataLists["safeList"] = safeAllergicLists.safeList;
 
   if (dataLists.allergicList && dataLists.safeList) {
-    let excluseLists = createExclusiveSafeAllergicLists(dataLists);
-    dataLists["safeListWithoutAllergic"] =  excluseLists.safeListWithoutAllergic;
-    dataLists["allergicListWithoutSafe"] = excluseLists.allergicListWithoutSafe;
+    let exclusiveLists = createExclusiveSafeAllergicLists(dataLists);
+    dataLists["safeListExclusive"] =  exclusiveLists.safeListExclusive;
+    dataLists["allergicListExclusive"] = exclusiveLists.allergicListExclusive;
   }
 
   return dataLists;
 }
 
 function createExclusiveSafeAllergicLists (dataLists) {
-  let allergicListWithoutSafe = dataLists.allergicList;
-  let safeListWithoutAllergic = dataLists.safeList;
+  let allergicListExclusive = jQuery.extend(true, {}, dataLists.allergicList);
+  let safeListExclusive = jQuery.extend(true, {}, dataLists.safeList);
 
-  for (let key in lists.allergicList) {
-    if (key in lists.safeList) {
-      delete allergicListWithoutSafe[key];
+  for (let key in dataLists.allergicList) {
+    if (key in dataLists.safeList) {
+      delete allergicListExclusive[key];
     }
   }
 
-  for (let key in lists.safeList) {
-    if (key in lists.allergicList) {
-      delete safeListWithoutAllergic[key];
+  for (let key in dataLists.safeList) {
+    if (key in dataLists.allergicList) {
+      delete safeListExclusive[key];
     }
   }
 
   return {
-    safeListWithoutAllergic: safeListWithoutAllergic,
-    allergicListWithoutSafe: allergicListWithoutSafe
+    safeListExclusive: safeListExclusive,
+    allergicListExclusive: allergicListExclusive
   }
 }
 
 function createSharedList (csvDataArray) {
   let sharedList = new Object();
 
+  //includes TBD, good, && bad
   try {
     csvDataArray.forEach(function (csvObject) {
       sharedList = iterateThroughDictionary(csvObject["Ingredients"], sharedList);
@@ -80,24 +81,30 @@ function createSafeAllergicLists(csvDataArray) {
     safeList: safeList,
     allergicList: allergicList
   };
-
 }
 
 function iterateThroughDictionary(ingredients, dict) {
   try {
-    //splits by comma, space, and removes empty string
-    let ingredientsArray = ingredients.split(/[,]+/).filter(Boolean);
 
-   ///^[ A-Za-z0-9_@./#&+-]*$/
+    //regex for splitting by just comma, while ignoring parens: /,(?![^(]*\))/
+    //splits by a "space and comma" e.g. "foo, bar, xyz" vs "foo,bar,xyz", while ignoring parens
+    let regex = /,(?![^(]*\)) /;
+
+    //remove zero width space
+    ingredients = ingredients.replace(/[\u200B-\u200D\uFEFF]/g, '');
+    let ingredientsArray = ingredients.split(regex).filter(Boolean);
+
     ingredientsArray.forEach(function (listIngredient) {
-      var ingredient = listIngredient.toLowerCase();
+      let ingredient = listIngredient.toLowerCase().trim();
 
-      if (waterList.includes(ingredient)) {
+      //replacing any aliases of water with just water
+      if (waterList.includes(ingredient) || waterList.includes(ingredient.split(" ")[0])) {
         ingredient = "water";
       }
 
-      dict[ingredient] = dict[ingredient] ? dict[ingredient]+1 : 1;
+      dict[ingredient] = (dict[ingredient] || 0) + 1;
     });
+
   } catch (error) {
     console.log(error)
     //will do this twice unless i do this check outside
