d9121c5eb0c9db53a274da9a6332087a006307a8
schang1 <schang1@godaddy.com>
2019-09-04 19:58:00 -0400

    add asynch

--- scripts/handleDataScript.js
+++ scripts/handleDataScript.js
@@ -1,3 +1,14 @@
+/*
+handleData:
+
+  Purpose: top level function -- creates the various lists
+
+  - Given a csvDataArray grabbed from a .csv
+    - Instantiate the empty dataLists object
+    - Call createSafeAllergicLists to fetch both the safeList and the allergicList
+    - Set key and value in dataLists
+    - Check that both the allergicList and safeList exists before creating exclusive lists
+*/
 function handleData(csvDataArray) {
   let dataLists = {};
   let safeAllergicLists = createSafeAllergicLists(csvDataArray);
@@ -15,6 +26,53 @@ function handleData(csvDataArray) {
   return dataLists;
 }
 
+/*
+quickCompare:
+
+  Purpose: for the Quick Compare tool. Given two columns, whether comma delimited ingredients or a CosDna webpage,
+  compare and give both the shared ingredients and the not-shared ingredients
+
+  - Given two ingredients columns (strings)
+    - Call getIngredients on both strings
+    - Merge both and pass that into createSharedList and set that as one list
+    - Create a new list called nonSharedList and run the same logic as
+      createExclusiveSafeAllergicLists (possibly refactor)
+    - Return both lists
+*/
+function quickCompare () {
+  // here are the ingredients these two columns share:
+  // here are the ingredients these two columns do not share
+}
+
+/*
+getIngredients:
+  Purpose: check if there's a website in the ingredients column instead of actual comma
+  delimited ingredients and calls the appropriate web scraping functions
+
+  - Given an ingredientsColumn
+    - Check if it's a CosDna website
+    - If yes, call fetchIngredientsFromCosdna and return ingredients
+    - If not, return ingredients as is
+*/
+function getIngredients (ingredientsColumn) {
+
+  if (isCosdna(ingredientsColumn)) {
+    return getDomFromCosdna(ingredientsColumn);
+  } else {
+    return $.Deferred().resolve(ingredientsColumn);
+  }
+  //this should be called before iterateThroughDictionary and is what is passed into iterateThroughDictionary
+}
+
+/*
+createExclusiveSafeAllergicLists:
+  Purpose: to create lists that are exclusive from each other: e.g. safeListExclusive won't
+  have any of the allergic ingredients on them and vice versa
+
+  - Given an object of dataLists with allergicList and safeList
+    - Make a deep copy of both lists and iterate through each list and delete what is not in the other
+    - Repeat again for second list
+*/
 function createExclusiveSafeAllergicLists (dataLists) {
   let allergicListExclusive = jQuery.extend(true, {}, dataLists.allergicList);
   let safeListExclusive = jQuery.extend(true, {}, dataLists.safeList);
@@ -37,13 +95,25 @@ function createExclusiveSafeAllergicLists (dataLists) {
   }
 }
 
+/*
+createSharedList:
+  Purpose: to created a shared list from the .csv object
+
+  - Given a csvDataArray grabbed from a .csv
+    - Creates an empty object instantiated as sharedList
+    - Iterates through each row and passes the ingredients column to iterateThroughDictionary
+      regardless of result
+    - Sets it to sharedList
+*/
 function createSharedList (csvDataArray) {
   let sharedList = new Object();
 
   //includes TBD, good, && bad
   try {
     csvDataArray.forEach(function (csvObject) {
-      sharedList = iterateThroughDictionary(csvObject["Ingredients"], sharedList);
+      getIngredients(csvObject["Ingredients"]).done(function (data) {
+        sharedList = iterateThroughDictionary(data, sharedList);
+      })
     })
   } catch (error) {
     console.log(error);
@@ -53,6 +123,19 @@ function createSharedList (csvDataArray) {
   return sharedList;
 }
 
+/*
+createSafeAllergicLists:
+
+  Purpose: to create safe and allergic lists from the .csv object
+
+  - Given a csvDataArray grabbed from a .csv
+      - Creates two empty objects instantiated as safeList and allergicList
+      - Iterates through each row and lowercases the "result" key for comparison
+      - If result is not "tbd", and is "safe", then pass ingredients iterateThroughDictionary
+        and put all the ingredients into a safeList
+      - If result is not "tbd", and is "bad" or "allergic", the pass ingredients
+        iterateThroughDictionary and put all ingredients into an allergicList
+*/
 function createSafeAllergicLists(csvDataArray) {
   let safeList = new Object();
   let allergicList = new Object();
@@ -83,6 +166,16 @@ function createSafeAllergicLists(csvDataArray) {
   };
 }
 
+/*
+iterateThroughDictionary:
+
+ Purpose: to sanitize ingredients, lowercase them, and return the counts of each ingredient found
+
+ - Given an ingredients column and a dictionary
+    - Sanitizes the ingredients column using regex and removing zero width spaces
+    - Loops through each ingredient, lowercases them, and replaces any ingredient that might just be "water" with simply water
+    - Then puts the ingredient in a dictionary and increments the count for each time the ingredient is found
+*/
 function iterateThroughDictionary(ingredients, dict) {
   try {
 
