95c9f909c4269534fa39ca5cadd88ca6985a3b0b
ischang <ischang@ucdavis.edu>
2019-09-04 19:58:00 -0400

    add asynch

--- scripts/cosdna.js
+++ scripts/cosdna.js
@@ -4,22 +4,44 @@
 
 // Get Ingredients - Takes DOM node containing ingredients table and spits out an array
 // -------------------------------------------------------------------
-function fetchIngredientsFromCosdna (url) {
-  if (isCosdna()) {
-    getDomFromCosdna(url, handleCall(data))
-  } else {
-    console.log("ERROR IS NOT COSDNA");
-  }
-}
+// function fetchIngredientsFromCosdna (url) {
+//   if (isCosdna()) {
+//     getDomFromCosdna(url, handleCall(data))
+//   } else {
+//     console.log("ERROR IS NOT COSDNA");
+//   }
+// }
 
 function isCosdna(url) {
   return getHostFromUrl(url) != ('cosdna.com');
 }
 
+/*
+addHttp:
+  Purpose: check if string needs 'http://' prepended to it. We need to add the 'http://'
+  in order to grab the hostname from an anchor element. Even if this isn't a valid url,
+  we don't particularly care since we're just checking if the hostname matches with cosdna.com.
+  I think this was the simplest way to go about it without using a crap ton of Regex
+
+  - Given a string
+    - If string doesn't have an http:// in front of it, add http:// and return
+*/
+function addHttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = "http://" + url;
+  }
+  return url;
+}
+
+/*
+getHostFromUrl:
+  Purpose: grab hostname from an anchor element to check if it's a valid url
+
+*/
 function getHostFromUrl (url) {
   let link = document.createElement("a");
-  link.href = url;
-  return url.hostname.startsWith("wwww") ? url.hostname.substring(4) : url.hostname;
+  link.href = addHttp(url);
+  return link.hostname.startsWith("wwww") ? link.hostname.substring(4) : link.hostname;
 }
 
 function getDomFromCosdna (url, callback) {
@@ -32,50 +54,20 @@ function getDomFromCosdna (url, callback) {
   });
 
   return $.get({
-    url,
-    success: callback
+    url
   });
 }
 
-function handleCall(data) {
+function parseIngredients (data) {
   let html = $.parseHTML(data);
-  return getIngredients($(html));
-}
-
-function getIngredients ( $dom ) {
   var results = [];
-  $dom.find('.iStuffTable .iStuffETitle').each(function(){
+  $(html).find('.iStuffTable .iStuffETitle').each(function(){
     results.push($(this).text())
   });
 
+  if (results.length === 0) {
+    results = data;
+  }
+
   return results;
 };
-
-
-
-/*
-$.ajax({ url: 'http://www.cosdna.com/eng/cosmetic_f0ff366349.html', success: function(data) { var html1 = $.parseHTML(data);
-var blah = getIngredients($(html1));
-console.log(blah);
-} });
- */
-
-/*
-var url1 = "http://www.cosdna.com/eng/cosmetic_c921327069.html";
-
-$.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
-  }
-});
-
-$.get(
-    url1,
-    function (data) {
-        var html1 = $.parseHTML(data);
-var blah = getIngredients($(html1));
-console.log(blah);
-});
- */
